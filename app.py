1fb3b1021bdd5c2bea93b8b099a8ce2b5d79ce29
ari <ari.me>
2026-07-04 21:44:14 +0700

    setup streamlit

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import streamlit as  st
 import joblib
 import pandas as pd
+import numpy as np
 
-# Judul
-st.title("Aplikasi Rumah Makan Padang Sinar Minang")
+st.set_page_config(
+    page_title="Hahahahaha",
+    page_icon=":smiley:",
+    layout="wide",
+)
+
+st.title("Hahahahaha")
+
+
+
