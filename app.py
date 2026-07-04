99c95badbbedfd2bb68feb58d03d46e0a38c47fa
ari <ari.me>
2026-07-04 21:52:04 +0700

    membuat sidebar

--- app.py
+++ app.py
@@ -11,11 +11,36 @@ st.set_page_config(
 
 st.title("Hahahahaha")
 
-
+# LOAD MODEL
 rf_model = joblib.load("models/rf_model.pkl")
 scaler = joblib.load("models/scaler_rf.pkl")
 
+st.sidebar.header("Input Data")
+region = st.sidebar.selectbox(
+    "Region",
+    [
+        "Garut",
+        "Indramayu",
+        "Karawang",
+        "Subang",
+        "Tasikmalaya"
+    ]
+)
 
+land_area = st.sidebar.number_input(
+    "Land Area (in square meters)",
+    min_value=100.0,
+)
+seed_cost = st.sidebar.number_input(
+    "Seed Cost (Rupiah)",
+)
+fertilizer_cost = st.sidebar.number_input(
+    "Fertilizer Cost (Rupiah)",
+)
+
+labor_cost = st.sidebar.number_input(
+    "Labor Cost (Rupiah)",
+)
 
 
 
