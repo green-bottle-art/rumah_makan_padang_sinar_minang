578aa0bcdd105cba852cd2d79ca7dd1b1c782819
ari <ari.me>
2026-07-04 22:21:32 +0700

    Perbaikan

--- app.py
+++ app.py
@@ -41,6 +41,20 @@ fertilizer_cost = st.sidebar.number_input(
 labor_cost = st.sidebar.number_input(
     "Labor Cost (Rupiah)",
 )
+land_lease = st.sidebar.number_input(
+    "Land Lease Value (IDR)",
+    min_value=0.0
+)
+
+pesticide_cost = st.sidebar.number_input(
+    "Pesticide Cost (IDR)",
+    min_value=0.0
+)
+
+equipment_cost = st.sidebar.number_input(
+    "Equipment Rent Value (IDR)",
+    min_value=0.0
+)
 
 REGION_MAP = {
     "Garut": 0,
@@ -50,43 +64,45 @@ REGION_MAP = {
     "Tasikmalaya": 4
 }
 
-#### Prediksi
-# x = np.array([[
-#     land_area,
-#     seed_cost,
-#     fertilizer_cost,
-#     labor_cost,
-#     REGION_MAP[region]
-# ]])
 
-import pandas as pd
+
+
+total_cost = (
+    land_lease
+    + labor_cost
+    + seed_cost
+    + fertilizer_cost
+    + pesticide_cost
+    + equipment_cost
+)
+
+cost_per_m2 = total_cost / land_area
+
+fertilizer_ratio = fertilizer_cost / land_area
+
+labor_ratio = labor_cost / land_area
 
 df_input = pd.DataFrame({
     "Land area (m2)": [land_area],
     "Land lease value (IDR)": [land_lease],
-    "Labor cost (IDR)": [labor],
-    "Seed purchase value (IDR)": [seed],
-    "Fertilizer purchase value (IDR)": [fertilizer],
-    "Pesticide purchase value (IDR)": [pesticide],
-    "Equipment rent value (IDR)": [equipment],
+    "Labor cost (IDR)": [labor_cost],
+    "Seed purchase value (IDR)": [seed_cost],
+    "Fertilizer purchase value (IDR)": [fertilizer_cost],
+    "Pesticide purchase value (IDR)": [pesticide_cost],
+    "Equipment rent value (IDR)": [equipment_cost],
     "region_encoded": [REGION_MAP[region]],
     "cost_per_m2": [cost_per_m2],
     "fertilizer_ratio": [fertilizer_ratio],
     "labor_ratio": [labor_ratio],
 })
 
-df_scaled = scaler.transform(df_input)
-
-prediction = rf_model.predict(df_scaled)
-
-
-# x = scaler.transform(x)
-# prediction = rf_model.predict(x)
-
-
 if st.button("Predict"):
+
+    df_scaled = scaler.transform(df_input)
+
+    prediction = rf_model.predict(df_scaled)
+
     st.metric(
         "Predicted Rice Production",
-        f"{prediction[0]:,.2f} kg"
+        f"Rp {prediction[0]:,.0f}"
     )
-
